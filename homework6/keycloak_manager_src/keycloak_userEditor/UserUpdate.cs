f76336c150ceddb027d4fb939e3c7384d3c3f576
Роман Агафонов <reagafonov@yandex.ru>
2025-04-25 23:15:16 +0300

    user service

---
+++ homework6/keycloak_manager_src/keycloak_userEditor/UserUpdate.cs
@@ -0,0 +1,16 @@
+namespace keycloak_userEditor;
+
+public class UserUpdate
+{
+    public string FirstName { get; set; }
+
+    public string LastName { get; set; }
+
+    public string Email { get; set; }
+
+    public string AvatarUrl { get; set; }
+
+    public int Age { get; set; }
+
+    public string Login { get; set; }
+}
