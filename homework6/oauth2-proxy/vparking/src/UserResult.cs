05f4404b8c42b015872a1f3e12f3f4c0ae41180b
Роман Агафонов <reagafonov@yandex.ru>
2025-05-03 00:33:03 +0300

    работает openid

---
+++ homework6/oauth2-proxy/vparking/src/UserResult.cs
@@ -0,0 +1,18 @@
+namespace keycloak_userEditor;
+
+public class UserResult
+{
+    public string Id { get; set; }
+    
+    public string FirstName { get; set; }
+
+    public string LastName { get; set; }
+
+    public string Email { get; set; }
+
+    public string AvatarUrl { get; set; }
+
+    public int Age { get; set; }
+
+    public string Login { get; set; }
+}
